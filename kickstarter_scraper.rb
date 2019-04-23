d260c9f096aa3a84ab9dc8ecaf80dc0cf1c00c96
Gerardo Manzo <programmingmanzo@gmail.com>
2019-04-23 05:01:49 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,13 +4,14 @@ require 'pry'
 
 class Scraper 
   
-def create_project_hash
-  # write your code here
-  html = File.read('fixtures/kickstarter.html')
+  def create_project_hash
+    # write your code here
+    html = File.read('fixtures/kickstarter.html')
   
-  kickstarter = Nokogiri::HTML(html)
+    kickstarter = Nokogiri::HTML(html)
   
-  binding.pry 
+    binding.pry 
+  end 
   
 end
 
