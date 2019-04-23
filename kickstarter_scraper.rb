14a295c6a87b27695f2abcf05d7d14b2d120b9f4
Gerardo Manzo <programmingmanzo@gmail.com>
2019-04-23 05:23:51 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -18,11 +18,11 @@ require 'pry'
     
     projects = {}
     
-     # Iterate through the projects
+    # Iterate through the projects
     kickstarter.css("li.project.grid_4").each do |project|
     projects[project] = {}
   end
-  
+  # return the projects hash
   projects 
   
     binding.pry 
