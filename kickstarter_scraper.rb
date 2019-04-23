4bd50c1a8de63124f8ae63663055e358a0678f2e
Gerardo Manzo <programmingmanzo@gmail.com>
2019-04-23 05:26:49 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -18,10 +18,8 @@ require 'pry'
     
     projects = {}
     
-   kickstarter.css("li.project.grid_4").each do |project|
-      title = project.css("h2.bbcard_name strong a").text
-      projects[title.to_sym] = {}
-    end
+    
+    
   
   # return the projects hash
   projects 
